4deb2edf83d826808d245be061c43a261ffa46a8
Japhet Ong <jong@pacificpetroleum.com.au>
2023-10-26 04:04:26 +0000

    Removed QuoteList.kt

--- app/src/main/java/ph/edu/auf/navbarlessonquotes/helpers/QuotesGenerator.kt
+++ app/src/main/java/ph/edu/auf/navbarlessonquotes/helpers/QuotesGenerator.kt
@@ -30,33 +30,14 @@ class QuotesGenerator private constructor(private val quotes: List<String>) {
             "To love and be loved is to feel the sun from both sides. - David Viscott"
         )
 
-        private val motivationalQuotes = listOf(
-            "Believe you can and you're halfway there. -Theodore Roosevelt",
-            "The only way to do great work is to love what you do. -Steve Jobs",
-            "Don't watch the clock; do what it does. Keep going. -Sam Levenson",
-            "Success is not final, failure is not fatal: It is the courage to continue that counts. -Winston Churchill",
-            "The future depends on what you do today. -Mahatma Gandhi",
-            "The only place where success comes before work is in the dictionary. -Vidal Sassoon",
-            "It does not matter how slowly you go as long as you do not stop. -Confucius",
-            "Your time is limited, don't waste it living someone else's life. -Steve Jobs",
-            "The only limit to our realization of tomorrow will be our doubts of today. -Franklin D. Roosevelt",
-            "I find that the harder I work, the more luck I seem to have. -Thomas Jefferson"
-
-        )
 
         fun generateLoveQuotes(): QuoteBuilder{
             return QuoteBuilder().apply { quotes.addAll(loveQuotes) }
         }
 
-        fun generateMotivationalQuotes(): QuoteBuilder{
-            return QuoteBuilder().apply { quotes.addAll(motivationalQuotes) }
-        }
 
         fun generateAllQuotes(): QuoteBuilder{
-            val allQuotes = mutableListOf<String>()
-            allQuotes.addAll(loveQuotes)
-            allQuotes.addAll(motivationalQuotes)
-            return QuoteBuilder().apply { quotes.addAll(allQuotes) }
+            return QuoteBuilder().apply { quotes.addAll(loveQuotes) }
         }
     }
 
